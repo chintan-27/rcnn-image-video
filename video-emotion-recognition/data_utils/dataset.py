ce54ee8b30d2b768f8c0294a2cf1d5865d7a2151
chintan-27 <chintan.acharya27@gmail.com>
2025-10-07 21:12:39 -0400

    Initial Setup

--- video-emotion-recognition/data_utils/dataset.py
+++ video-emotion-recognition/data_utils/dataset.py
@@ -1,1 +1,13 @@
+import os
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
 
+class CKVideoDataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
